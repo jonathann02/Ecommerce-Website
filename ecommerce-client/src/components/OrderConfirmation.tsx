8b8b32c56c948e2fed9a2059e28377ef03a84962
Jonathan_Norlin <jonathan.norlin@icloud.com>
2025-04-09 02:24:28 +0200

    orderconfirmation url change

--- ecommerce-client/src/components/OrderConfirmation.tsx
+++ ecommerce-client/src/components/OrderConfirmation.tsx
@@ -22,7 +22,7 @@ export const OrderConfirmation = () => {
         const sessionId = params.get("session_id")
 
         if (sessionId) {
-            fetch(`https://ecommerce-api-new-eight.vercel.app/${sessionId}`)
+            fetch(`https://ecommerce-api-new-eight.vercel.app/order-confirmation?session_id=${sessionId}`)
             .then(res => res.json())
             .then(data => {
                 setOrder(data)
