8a5e128c81115d4377c0078fd509bc944592585e
Jonathan_Norlin <jonathan.norlin@icloud.com>
2025-04-11 00:01:07 +0200

    change to hashrouter to make order conf page work

--- ecommerce-client/src/main.tsx
+++ ecommerce-client/src/main.tsx
@@ -2,15 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { CartProvider } from './contexts/CartContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <CartProvider>
       <App />
     </CartProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
+
+// BrowserRouter
